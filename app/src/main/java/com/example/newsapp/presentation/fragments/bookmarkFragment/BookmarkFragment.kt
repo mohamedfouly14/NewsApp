e5d6667a2967d09f926289994869d10c2fdd60c4
DELL <mohamedfouly14@gmail.com>
2023-03-26 02:50:19 +0200

    beforse details 2

--- app/src/main/java/com/example/newsapp/presentation/fragments/bookmarkFragment/BookmarkFragment.kt
+++ app/src/main/java/com/example/newsapp/presentation/fragments/bookmarkFragment/BookmarkFragment.kt
@@ -1,8 +1,55 @@
 package com.example.newsapp.presentation.fragments.bookmarkFragment
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.azmiradi.news.presentation.ui_handlers.Handlers
+import com.example.newsapp.R
+import com.example.newsapp.Utils.showToast
 import com.example.newsapp.databinding.FragmentBookmarkBinding
 import com.example.newsapp.databinding.FragmentHomeBinding
+import com.example.newsapp.presentation.adaptors.LatestNewsAdapter
 import com.example.newsapp.presentation.fragments.BaseFragment
+import java.util.logging.Handler
+import javax.inject.Inject
 
-class BookmarkFragment:BaseFragment<FragmentBookmarkBinding>(FragmentBookmarkBinding::inflate) {
+class BookmarkFragment:BaseFragment<FragmentBookmarkBinding>(FragmentBookmarkBinding::inflate),Handlers {
+
+    @Inject
+    lateinit var latestNewsAdapter: LatestNewsAdapter
+
+    private val viewModel: BookmarkViewModel by viewModels()
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun setupAdaptor() {
+        TODO("Not yet implemented")
+    }
+
+    override fun handleUI() {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeData() {
+        viewModel.allNewsStates.observe(requireActivity()){
+            if (it.isEmpty()) {
+                binding.errorMassage.visibility = View.VISIBLE
+                latestNewsAdapter.differ.submitList(ArrayList())
+            } else {
+                binding.errorMassage.visibility = View.GONE
+                latestNewsAdapter.differ.submitList(it)
+            }
+        }
+        viewModel.deleteArticle.observe(requireActivity()) {
+            if (it) {
+                viewModel.geBookmark()
+                requireContext().showToast(getString(R.string.article_deleted))
+            } else {
+                requireContext().showToast(getString(R.string.article_not_deleted))
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/example/newsapp/presentation/fragments/bookmarkFragment/BookmarkViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.newsapp.presentation.fragments.bookmarkFragment
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.newsapp.data.model.Article
+import com.example.newsapp.domain.use_cases.DeleteArticleUseCase
+import com.example.newsapp.domain.use_cases.GetLocalArticlesUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+class BookmarkViewModel @Inject constructor(
+    private val getLocalArticlesUseCase: GetLocalArticlesUseCase,
+    private val deleteArticleUseCase: DeleteArticleUseCase
+) :ViewModel(){
+    private var job :Job?=null
+    private val _allNewsStates=MutableLiveData<List<Article>>(ArrayList())
+    var allNewsStates = _allNewsStates
+    private val _deleteArticle = MutableLiveData<Boolean>()
+    val deleteArticle: LiveData<Boolean> = _deleteArticle
+
+    init {
+        geBookmark()
+    }
+
+    fun geBookmark(){
+        job?.cancel()
+        job = getLocalArticlesUseCase().onEach {
+            _allNewsStates.value=it
+
+        }.launchIn(viewModelScope)
+    }
+    fun deleteArticle(article: Article){
+        job?.cancel()
+        job=deleteArticleUseCase(article).onEach {
+            _deleteArticle.value=it
+        }.launchIn(viewModelScope)
+    }
+}
